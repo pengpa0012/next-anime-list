7b6944006763681012b921888089c739c7c57ca7
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-05 19:30:44 +0800

    add hero banner layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,19 @@
 export default function Home() {
   return (
     <main>
-      <h1>Hello World</h1>
+      <section className="main-cover flex items-center">
+        <div className="p-6 max-w-4xl">
+          <h1 className="text-7xl font-bold">Title Here</h1>
+          <p className="text-xl py-4">Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptatum aut voluptatem ex deserunt laboriosam molestias asperiores eligendi, magnam quibusdam nisi. Ut commodi itaque quis? Autem?</p>
+          <div className="flex gap-4">
+            <button className="bg-white font-bold rounded-md py-2 px-8 text-2xl">Play</button>
+            <button className="font-bold rounded-md py-2 px-8 text-2xl">More Info</button>
+          </div>
+        </div>
+      </section>
+      <section className="bg-[#141414] text-white p-4">
+        <h1>Carousel here</h1>
+      </section>
     </main>
   )
 }
---
+++ src/components/Navbar.tsx
@@ -0,0 +1,16 @@
+import Link from 'next/link'
+import React from 'react'
+
+export const Navbar = () => {
+  return (
+    <div className="flex justify-between items-center p-4 bg-transparent">
+      <ul className='flex gap-4'>
+        <Link href="/" >Home</Link>
+        <Link href="/about" >About</Link>
+        <li>Home</li>
+        <li>Home</li>
+      </ul>
+      <p>Ttest</p>
+    </div>
+  )
+}
