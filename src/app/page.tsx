7a9065dedfeaeab9e6055db738fff4a3bbd2fb7c
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-09 12:59:39 +0800

    added top anime carousel

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,9 @@
 import { Video } from "@/components/Video"
 import { fetchData } from "@/utilities/fetch"
+import Image from "next/image"
 
 export default async function Home() {
-  const data = await fetchData("https://api.jikan.moe/v4/anime/1")
+  const {data} = await fetchData("https://api.jikan.moe/v4/anime?min_score=9")
 
   return (
     <main>
@@ -18,13 +19,19 @@ export default async function Home() {
         <Video id="XSaBR58f7I0" />
       </section>
       <section className="bg-[#141414] text-white p-4">
-        <div className="-translate-y-[80px] flex gap-10 overflow-x-auto space-x-8 custom-scroll pb-4">
-          {
-            [1,2,3,4,5,7,8,9,10,11,12,33,42].map(item => (
-              <div className="w-[300px] h-[140px] bg-white rounded-md flex-shrink-0" key={item}></div>
-            ))
-          }
+        <div className="-translate-y-[150px]">
+          <h2 className="text-3xl mb-4 font-bold">Top Anime</h2>
+          <div className="flex gap-10 overflow-x-auto space-x-8 custom-scroll pb-4">
+            {
+              data.map((item: any, i: number) => (
+                <div className="max-h-[420px] overflow-hidden rounded-md flex-shrink-0" key={`card-${i}`}>
+                  <Image src={item.images.webp.image_url} alt={"image"} width={300} height={420}/>
+                </div>
+              ))
+            }
+          </div>
         </div>
+        
       </section>
     </main>
   )
