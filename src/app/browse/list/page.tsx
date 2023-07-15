6a3a05d9bad9ce9ed29a080fa8c81bfe4b615962
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-16 00:35:34 +0800

    added nested route

--- src/app/browse/list/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from 'react'
-
-export const page = () => {
-  return (
-    <div>Genre List</div>
-  )
-}
--- src/app/browse/page.tsx
+++ src/app/browse/page.tsx
@@ -1,17 +1,20 @@
 import { genres } from '@/utilities/fetch'
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export default function About() {
  
   return (
-    <main className='min-h-screen bg-[#010101] mt-[-56px] text-white grid grid-cols-1 md:grid-cols-3 place-items-center gap-2'>
+    <main className='min-h-screen bg-[#010101] mt-[-56px] text-white grid grid-cols-1 md:grid-cols-3 place-items-center'>
       {
         genres.map(genre => (
-          <ul key={genre.name} className='w-full h-full bg-green-100/10 grid place-items-center relative genre-shadow overflow-hidden'>
-            <li className="text-3xl font-bold z-10">{genre.name}</li>
-            <Image src={genre.image} fill alt={'genre-img'} className='object-cover' />
-          </ul>
+          <div className='w-full h-full bg-green-100/10 grid place-items-center relative genre-shadow overflow-hidden'>
+            <Link href={'/browse/465'}>
+              <p key={genre.name} className="text-3xl font-bold z-10 relative">{genre.name}</p>
+              <Image src={genre.image} fill alt={'genre-img'} className='object-cover' />
+            </Link>
+          </div>
         ))
       }
     </main>
