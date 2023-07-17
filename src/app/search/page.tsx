6c2b952f9eb9df3372191fbaac800a399849d1d7
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-17 21:02:50 +0800

    added initial layout on search page

--- src/app/search/page.tsx
+++ src/app/search/page.tsx
@@ -1,7 +1,14 @@
+"use client"
 import React from 'react'
 
 export default function Search() {
   return (
-    <div>SEARCH</div>
+    <main className='min-h-screen bg-[#010101] mt-[-56px] text-white pt-56'>
+      <h1 className='text-center mb-4 text-3xl'>Search Anime</h1>
+      <form className="flex justify-center gap-2 max-w-2xl mx-auto" onSubmit={(e) => e.preventDefault()}>
+        <input type="text" placeholder='Ex. Attack on Titan' className='border border-white rounded-md p-2 bg-transparent w-full' />
+        <button type='submit' className='bg-white text-black rounded-md py-2 px-6'>Search</button>
+      </form>
+    </main>
   )
 }
