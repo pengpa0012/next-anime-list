14931b45801e12a0438159bd58517497ba44ac3e
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-19 08:47:17 +0800

    added hover on card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -3,7 +3,10 @@ import React from 'react'
 
 export const Card = ({item}: any) => {
   return (
-    <div className="w-[200px] h-[300px] overflow-hidden rounded-md flex-shrink-0 pointer-events-none select-none">
+    <div className="card w-[200px] h-[300px] overflow-hidden rounded-md flex-shrink-0 select-none">
+      <div className="card-details">
+        <h4 className="text-xl text-center">{item.title}</h4>
+      </div>
       <Image src={item.images.webp.image_url} alt={"image"} width={0} height={0} sizes="100vw" className="w-full h-full object-cover"/>
     </div>
   )
