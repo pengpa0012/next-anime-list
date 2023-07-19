6d480baefd09bbad6018e803eeab7bcbaa3db9dc
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-19 10:23:24 +0800

    update profile page

--- src/app/[profile]/page.tsx
+++ src/app/[profile]/page.tsx
@@ -2,6 +2,8 @@ import React from 'react'
 
 export default async function page({params}: any) {
   return (
-    <div>{params.profile}</div>
+    <main className='min-h-screen bg-[#010101] text-white mt-[-56px] grid place-items-center'>
+      <div>{params.profile}</div>
+    </main>
   )
 }
