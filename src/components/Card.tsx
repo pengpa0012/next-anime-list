c769dc2499033ed1d9829f38729c844bbf2fd97d
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-21 07:06:39 +0800

    added link on card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,13 +1,16 @@
 import Image from 'next/image'
+import Link from 'next/link'
 import React from 'react'
 
 export const Card = ({item}: any) => {
   return (
-    <div className="card w-[200px] h-[300px] overflow-hidden rounded-md flex-shrink-0 select-none">
-      <div className="card-details">
-        <h4 className="text-xl text-center">{item.title}</h4>
-      </div>
-      <Image src={item.images.webp.image_url} alt={"image"} width={0} height={0} sizes="100vw" className="w-full h-full object-cover"/>
+    <div className="card w-[200px] h-[300px] overflow-hidden rounded-md flex-shrink-0 select-none cursor-pointer">
+      <Link href={`/${item.mal_id}`}>
+        <div className="card-details">
+          <h4 className="text-xl text-center">{item.title}</h4>
+        </div>
+        <Image src={item.images.webp.image_url} alt={"image"} width={0} height={0} sizes="100vw" className="w-full h-full object-cover"/>
+      </Link>
     </div>
   )
 }
