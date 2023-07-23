2d0fe36cc58e17a0138058e8fe1689f66200e5d4
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-23 10:39:32 +0800

    update arrow

--- src/components/Carousel.tsx
+++ src/components/Carousel.tsx
@@ -3,21 +3,26 @@ import Image from 'next/image'
 import React from 'react'
 import { useState, useRef } from 'react'
 import { Card } from './Card'
+import { IoIosArrowBack, IoIosArrowForward } from 'react-icons/io';
 
 export const Carousel = ({animes}: any) => {
   const scrollContainerRef = useRef<any>(null)
+  const [disable, setDisable] = useState(false)
 
-  const onNextPage = () => {
-    scrollContainerRef.current.scrollLeft += scrollContainerRef.current.clientWidth / 2
-  }
-
-  const onPrevPage = () => {
-    scrollContainerRef.current.scrollLeft -= scrollContainerRef.current.clientWidth / 2
+  const onChangePage = (direction: string) => {
+    setDisable(true)
+    if(direction == "right") scrollContainerRef.current.scrollLeft += scrollContainerRef.current.clientWidth / 2
+    else scrollContainerRef.current.scrollLeft -= scrollContainerRef.current.clientWidth / 2
+    setTimeout(() => {
+      setDisable(false)
+    }, 500)
   }
 
   return (
     <div className="flex justify-center">
-      <button onClick={onPrevPage}>prev</button>
+      <button onClick={() => onChangePage("left")} className={`arrow pr-4 ${disable ? 'pointer-events-none' : 'pointer-events-all'}`}>
+        <IoIosArrowBack size={35} />
+      </button>
       <div className={`flex gap-10 overflow-x-auto space-x-8 custom-scroll pb-4 transition-250 scroll-smooth`}
         ref={scrollContainerRef}
       >
@@ -27,7 +32,9 @@ export const Carousel = ({animes}: any) => {
           ))
         }
       </div>
-      <button onClick={onNextPage}>next</button>
+      <button onClick={() => onChangePage("right")} className={`arrow pl-4 ${disable ? 'pointer-events-none' : 'pointer-events-all'}`}>
+        <IoIosArrowForward size={35} />
+      </button>
     </div>
   )
 }
