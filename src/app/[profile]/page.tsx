0e7bc927bd21b043dffefac18fbecaf8686a3788
Pengpa0012 <peng.ilikha@gmail.com>
2023-07-23 11:03:37 +0800

    added main cover on profile page

--- src/app/[profile]/page.tsx
+++ src/app/[profile]/page.tsx
@@ -1,11 +1,27 @@
+import { Video } from '@/components/Video'
 import { fetchData } from '@/utilities/fetch'
 import React from 'react'
+import { FaPlay } from 'react-icons/fa'
 
 export default async function page({params}: any) {
   const {data} = await fetchData(`https://api.jikan.moe/v4/anime/${params.profile}/full`)
+  console.log(data.trailer.images.maximum_image_url)
   return (
-    <main className='min-h-screen bg-[#010101] text-white mt-[-56px] grid place-items-center'>
-      <div>{data.title}</div>
+    <main>
+      <section className="main-cover flex items-center text-white relative">
+        <div className="p-6 max-w-4xl">
+          <h1 className="text-7xl font-bold">{data.title}</h1>
+          <p className="text-xl my-4 text-limit">{data.background ?? data.synopsis}</p>
+          <div className="flex gap-4">
+            <button className="bg-white font-bold rounded-md py-2 px-8 text-2xl text-black flex items-center gap-2">
+              <FaPlay />
+              <span>Play</span>
+            </button>
+            <button className="font-bold rounded-md py-2 px-8 text-2xl">More Info</button>
+          </div>
+        </div>
+        <Video imgSrc={data.trailer.images.maximum_image_url ?? "https://via.placeholder.com/1280x1024"} />
+      </section>
     </main>
   )
 }
--- src/components/Video.tsx
+++ src/components/Video.tsx
@@ -18,7 +18,7 @@ export const Video = ({id, imgSrc}: any) => {
         iframeClassName='w-full h-full absolute inset-0 z-[-1]'
         onReady={(e) => e.target.playVideo()}
       /> */}
-      <img className='bg-black w-full h-full absolute inset-0 z-[-1]' src={imgSrc} />
+      <img className='bg-black w-full h-full absolute object-cover inset-0 z-[-1]' src={imgSrc} />
     </>
   )
 }
